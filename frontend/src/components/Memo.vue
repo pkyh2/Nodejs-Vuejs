d4012fed9d9816aa1c2f644564af3f788f8eacc0
pkyh2 <pkyh1@naver.com>
2022-06-19 21:29:12 +0900

    updating vue.config.js

---
+++ frontend/src/components/Memo.vue
@@ -0,0 +1,56 @@
+<template>
+	<div class="memo">
+		<div class="act">
+			<button class="btn btn-primary" @click="add()">+ 추가</button>
+		</div>
+		<ul>
+			<li v-for="(d, idx) in state.data" :key="idx">{{ d }}</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+import { reactive } from 'vue';
+import axios from 'axios';
+
+export default {
+	setup() {
+		const state = reactive({
+			data: [],
+		});
+		const add = () => {
+			state.data.push('추가된 메모 내용');
+		};
+
+		axios.get('http://localhost:3000/memos').then(res => {
+			console.log(res);
+		});
+
+		return { state, add };
+	},
+};
+</script>
+
+<style lang="scss" scoped>
+// .memo button {
+// 	text-align: center;
+// }
+.memo {
+	.act {
+		padding: 10px 10px 5px 5px;
+		text-align: right;
+	}
+
+	ul {
+		list-style: none;
+		padding: 15px 0;
+		margin: 0;
+
+		li {
+			padding: 15px;
+			margin: 15px;
+			border: 1px solid #eee;
+		}
+	}
+}
+</style>
